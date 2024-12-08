ede2c18aef030bc798ad2c345215b67009e87958
Walter <sarvarbek200028@gmail.com>
2024-12-09 04:24:49 +0900

    fix: modify view module to record visits

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -2,8 +2,6 @@ import { Field, InputType } from "@nestjs/graphql";
 import { IsNotEmpty, IsOptional, Length } from "class-validator";
 import { MemberAuthType, MemberType } from "../../enums/member.enum";
 
-
-
 @InputType()
 export class MemberInput {
     @IsNotEmpty()

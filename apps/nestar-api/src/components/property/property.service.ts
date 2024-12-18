ea72162f5369876ee4c06cff248e185c98fea427
Walter <sarvarbek200028@gmail.com>
2024-12-18 18:55:06 +0900

    feat: develop boardArticle releted GraphQL APIs

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -61,18 +61,7 @@ export class PropertyService {
     return targetProperty;
    }
    
-   public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-    const { _id, targetKey, modifier } = input;
-    return await this.propertyModel
-    .findByIdAndUpdate(
-        _id,
-        { $inc: { [targetKey]: modifier } },
-        {
-            new: true,
-        },
-    )
-    .exec();
-   }
+
 
    public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
     let { propertyStatus, soldAt, deletedAt } = input;
@@ -262,4 +251,17 @@ export class PropertyService {
 
     return result;
   }
+
+  public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
+    const { _id, targetKey, modifier } = input;
+    return await this.propertyModel
+    .findByIdAndUpdate(
+        _id,
+        { $inc: { [targetKey]: modifier } },
+        {
+            new: true,
+        },
+    )
+    .exec();
+   }
 } 

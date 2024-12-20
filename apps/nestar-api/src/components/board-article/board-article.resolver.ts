99576ec87d83229b9ae526357c91aa2948a877da
Walter <sarvarbek200028@gmail.com>
2024-12-20 17:42:43 +0900

    feat: develop likeTargetBoardArticle GraphQL api

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -60,6 +60,16 @@ export class BoardArticleResolver {
         return await this.boardArticleService.updateBoardArticle(memberId, input);
     }
 
+    @UseGuards(AuthGuard)
+    @Mutation(() => BoardArticle)
+    public async likeTargetBoardArticle(
+        @Args("articleId") input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log("Mutation: likeTargetBoardArticle");
+        const likeRefId = shapeIntoMongoObjectId(input);
+        return await this.boardArticleService.likeTargetBoardArticle(memberId, likeRefId)
+    }
 
     /**   ADMIN   */
 
--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -11,6 +11,9 @@ import { ViewGroup } from "../../libs/enums/view.enum";
 import { StatisticModifier, T } from "../../libs/types/common";
 import { BoardArticleUpdate } from "../../libs/dto/board-article/board-article.update";
 import { lookupMember, shapeIntoMongoObjectId } from "../../libs/config";
+import { LikeInput } from "../../libs/dto/like/like.input";
+import { LikeGroup } from "../../libs/enums/like.enum";
+import { LikeService } from "../like/like.service";
 
 
 
@@ -20,6 +23,7 @@ export class BoardArticleService {
     @InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
     private readonly memberService: MemberService,
     private readonly viewService: ViewService,
+    private readonly likeService: LikeService,
 ) {}
 
 public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
@@ -128,6 +132,29 @@ public async getBoardArticle(memberId: ObjectId, articleId: ObjectId): Promise<B
     .exec();
     if (!result.length) throw new InternalServerErrorException(Message.N0_DATA_FOUND);
     return result[0];
+  }
+
+  public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle> {
+    const target: BoardArticle = await this.boardArticleModel
+    .findOne({_id: likeRefId, articleStatus: BoardArticleStatus.ACTIVE})
+    .exec();
+    if(!target) throw new InternalServerErrorException(Message.N0_DATA_FOUND);
+
+    const input: LikeInput = {
+      memberId: memberId,
+      likeRefId: likeRefId,
+      likeGroup: LikeGroup.ARTICLE
+    };
+
+
+    const modifier: number = await this.likeService.toggleLike(input);
+    const result = await this.boardArticleStatsEditor({ 
+        _id: likeRefId, 
+        targetKey: 'articleLikes', 
+        modifier: modifier});
+
+    if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+    return result;
   }
 
 public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {

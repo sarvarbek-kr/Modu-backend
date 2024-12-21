ebc3a930a4a09c4a92332ec6a7d30346f239625d
Walter <sarvarbek200028@gmail.com>
2024-12-22 03:16:29 +0900

    feat: subscribe unssubscribe

--- apps/nestar-api/src/components/follow/follow.resolver.ts
+++ apps/nestar-api/src/components/follow/follow.resolver.ts
@@ -1,7 +1,35 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Follower, Followers } from '../../libs/dto/follow/follow';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class FollowResolver {
     constructor(private readonly followService: FollowService) {}
+
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => Follower)
+    public async subscribe(
+        @Args('input') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Follower> {
+        console.log('Mutation: subscribe');
+        const followingId = shapeIntoMongoObjectId(input);
+        return await this.followService.subscribe(memberId, followingId);
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => Follower)
+    public async unsubscribe(
+        @Args('input') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Follower> {
+        console.log('Mutation: unsubscribe');
+        const followingId = shapeIntoMongoObjectId(input);
+        return await this.followService.unsubscribe(memberId, followingId);
+    }
 }
--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -1,12 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Follower, Following } from '../../libs/dto/follow/follow';
 import { MemberService } from '../member/member.service';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class FollowService {
     constructor(@InjectModel("Follow") private readonly followModel: Model<Follower | Following>, 
     private readonly memberService: MemberService,
 ){}
+
+public async subscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    if (followerId.toString() === followingId.toString()) {
+        throw new InternalServerErrorException(Message.SELF_SUBSCRIPTION_DENIED);
+    }
+
+    const targetMember = await this.memberService.getMember(null, followingId);
+    if (!targetMember) throw new InternalServerErrorException(Message.N0_DATA_FOUND);
+
+    const result = await this.registerSubscription(followerId, followingId);
+
+    await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: 1 });
+    await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: 1 });
+
+    return result;
+}
+
+public async registerSubscription(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    try {
+        return await this.followModel.create({
+            followingId: followingId,
+            followerId: followerId,
+        });
+    } catch (err) {
+        console.log('Error, Service.model:', err.message);
+        throw new BadRequestException(Message.CREATE_FAILED);
+    }
+}
+
+public async unsubscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    const targetMember = await this.memberService.getMember(null, followingId);
+    if (!targetMember) throw new InternalServerErrorException(Message.N0_DATA_FOUND);
+
+    const result = await this.followModel.findOneAndDelete({
+        followingId: followingId,
+        followerId: followerId,
+    });
+    if (!result) throw new InternalServerErrorException(Message.N0_DATA_FOUND);
+
+    await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: -1 });
+    await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: -1 });
+
+    return result;
+}
+
 }
